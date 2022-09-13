a06026b627489cdaca858f98940bc01fa9598919
unknown <manojg.xworkz@gmail.com>
2022-09-13 21:19:35 +0530

    13 sep task

---
+++ ArgumentMethod.java
@@ -0,0 +1,63 @@
+class ArgumentMethod{
+
+	static void NameAndEmail(String Name,String Email)
+	{
+		System.out.println("entered NameAndEmail");
+		System.out.println("name entered:"+Name+" Email:"+Email);
+	}
+	
+	static void NameAndMobile(String Name,Long Mobile)
+	{
+		System.out.println("entered NameAndMobile");
+		System.out.println("name entered:"+Name+" Mobile:"+Mobile);
+	}
+	
+	static void NameAndAge(String Name,int Age)
+	{
+		System.out.println("entered NameAndAge");
+		System.out.println("name entered:"+Name+" Age:"+Age);
+	}
+	
+	static void NameAndAddress(String Name,String Address)
+	{
+		System.out.println("entered NameAndAddress");
+		System.out.println("name entered:"+Name+" Address:"+Address);
+	}
+	
+	static void NameAndEmailAndAddressAndAge(String Name,String Email,String Address,int Age)
+	{
+		System.out.println("entered NameAndEmailAndAddressAndAge");
+		System.out.println("name entered:"+Name+"Email:"+Email+" Address:"+Address+"Age"+Age);
+	}
+	
+	static void NameAndGender(String Name,String Gender)
+	{
+		System.out.println("entered NameAndGender");
+		System.out.println("name entered:"+Name+" Gender:"+Gender);
+	}
+	
+	static void NameAndWorking(String Name,Boolean Working)
+	{
+		System.out.println("entered NameAndWorking");
+		System.out.println("name entered:"+Name+" Working:"+Working);
+	}
+	
+	static void NameAndShirtSize(String Name,int ShirtSize)
+	{
+		System.out.println("entered NameAndShirtSize");
+		System.out.println("name entered:"+Name+" ShirtSize:"+ShirtSize);
+	}
+	
+	static void NameAndPantSize(String Name,int PantSize)
+	{
+		System.out.println("entered NameAndPantSize");
+		System.out.println("name entered:"+Name+" PantSize:"+PantSize);
+	}
+	
+	static void NameAndWeightAndHeight(String Name,int Weight, String Height)
+	{
+		System.out.println("entered NameAndWeightAndHeight");
+		System.out.println("name entered:"+Name+" Weight:"+Weight+" Height"+Height);
+	}
+
+}
